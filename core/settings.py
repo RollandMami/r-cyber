e64ac776965bd548972f63351e471280ecae513f
RollandMami <andrianavalonarolland@gmail.com>
2026-05-16 17:01:01 +0200

    base html

--- core/settings.py
+++ core/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,10 +121,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGE_CODE = 'fr-fr'
+
+USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ BASE_DIR / 'static', ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
